a410d2cbd8ad1da8314c477fc720ba6709e72426
Hiro Tanaka <HTanaka@lcs.on.ca>
2024-01-22 11:30:50 -0500

    Initial Commit

---
+++ TradingCardsMacOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TradingCardsMacOS
+//
+//  Created by 田中宏貴 on 2024-01-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TradingCardsMacOS/TradingCardsMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsMacOSApp.swift
+//  TradingCardsMacOS
+//
+//  Created by 田中宏貴 on 2024-01-22.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
