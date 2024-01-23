c6693f4c45737d614193b86fc8ef86eef3e12200
Hiro Tanaka <HTanaka@lcs.on.ca>
2024-01-23 14:46:05 -0500

    Got an image

--- TradingCardsMacOS/ContentView.swift
+++ TradingCardsMacOS/ContentView.swift
@@ -9,11 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Rectangle()
-                .frame(height: 3)
-                .padding()
+        ZStack{
+            Color.yellow
             
+            VStack {
+                
+                Spacer()
+                
+                Rectangle()
+                    .frame(height: 3)
+                  //.padding(.top)
+                    .padding(.leading)
+                    .padding(.trailing)
+                
+                Text("NOLAN")
+                    .fontWeight(.regular)
+                    .font(.system(.title, design: .serif))
+                    .lineSpacing(10.0)
+               
+                Rectangle()
+                    .frame(height: 3)
+                    .padding(.leading)
+                    .padding(.trailing)
+                
+                Text("RYAN")
+                    .fontWeight(.bold)
+                    .fontDesign(.serif)
+                    .font(.system(size: 40))
+                 
+        
+                    Image("BaseballPlayer")
+                    .resizable()
+                    .scaledToFit()
+                
+                
+                HStack{
+                    Rectangle()
+                        .frame(height: 5)
+                        .padding(.leading)
+                    
+                    Text("RANGERS•P")
+                        .fontWeight(.bold)
+                        .font(.system(size: 15))
+                    
+                    Rectangle()
+                        .frame(height: 5)
+                        .padding(.trailing)
+                        }
+                
+                Rectangle()
+                    .frame(height: 3)
+                    .padding(.leading)
+                    .padding(.trailing)
+                
+                Spacer()
+            }
+           
         }
         .frame(width: 300, height: 600)
     }
